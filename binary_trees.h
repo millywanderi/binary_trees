55856774b0cab5ccf2e96b410f53e9bf9a48b0d0
millywanderi <milliwanderi5@gmail.com>
2023-08-01 03:17:52 -0700

    header file

--- binary_trees.h
+++ binary_trees.h
@@ -36,6 +36,18 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
+size_t recurse_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+int full_recurse(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int recurse_balance(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 /* binary tree print prototypes */
 void binary_tree_print(const binary_tree_t *);
